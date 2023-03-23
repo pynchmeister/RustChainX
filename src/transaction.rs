6f07729ea07d74e32e81d13ce2ad444bc6d5f493
Amnot <acyclicgraphs@gmail.com>
2023-03-23 05:26:36 -0400

    Update transaction.rs

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,1 +1,9 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Transaction {
+    pub sender: String,
+    pub recipient: String,
+    pub amount: f64,
+}
 
