25ce1b8700af844a3d7179caac945f6099bbaf39
Amnot <acyclicgraphs@gmail.com>
2023-03-23 05:29:25 -0400

    Update pow.rs

--- src/pow.rs
+++ src/pow.rs
@@ -1,1 +1,24 @@
+use std::fmt::{Debug, Formatter};
+
+const DIFFICULTY_PREFIX: &str = "0000";
+
+pub fn proof_of_work(last_proof: u64) -> u64 {
+    let mut proof = 0;
+    while !is_valid_proof(last_proof, proof) {
+        proof += 1;
+    }
+    proof
+}
+
+pub fn is_valid_proof(last_proof: u64, proof: u64) -> bool {
+    let guess = format!("{}{}", last_proof, proof);
+    let guess_hash = crypto_hash::hex_digest(crypto_hash::Algorithm::SHA256, guess.as_bytes());
+    guess_hash.starts_with(DIFFICULTY_PREFIX)
+}
+
+impl Debug for ProofOfWork {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "ProofOfWork")
+    }
+}
 
