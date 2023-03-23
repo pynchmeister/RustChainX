b5fef0f0f798b958832e587780de9a548cf0b2d5
Amnot <acyclicgraphs@gmail.com>
2023-03-23 05:31:38 -0400

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,16 @@
+use crate::block::Block;
+use serde_json::json;
+
+pub fn hash_block(block: &Block) -> String {
+    let block_data = json!({
+        "index": block.index,
+        "timestamp": block.timestamp,
+        "transactions": block.transactions,
+        "proof": block.proof,
+        "previous_hash": block.previous_hash,
+    });
+
+    let serialized_block = serde_json::to_string(&block_data).unwrap();
+    crypto_hash::hex_digest(crypto_hash::Algorithm::SHA256, serialized_block.as_bytes())
+}
 
