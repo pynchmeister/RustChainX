26c3d14655a337e8e6b7332299e56fb1ec1fbc72
Amnot <acyclicgraphs@gmail.com>
2023-03-23 05:27:46 -0400

    Update block.rs

--- src/block.rs
+++ src/block.rs
@@ -1,1 +1,12 @@
+use crate::transaction::Transaction;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Block {
+    pub index: u64,
+    pub timestamp: u64,
+    pub transactions: Vec<Transaction>,
+    pub proof: u64,
+    pub previous_hash: String,
+}
 
